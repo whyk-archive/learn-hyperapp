35ab46889c35c2f46484c69f4d396edd75644470
WhyK <windchimeyk@gmail.com>
2019-12-07 19:10:28 +0900

    update: 複数のStateに対応したコードに変更

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import { h, app } from "hyperapp";
 
+const increment = state => ({...state, num: state.num + 1});
+const decrement = state => ({...state, num: state.num - 1});
+
 app({
-  init: 0,
+  init: {
+    title: 'カウント',
+    num: 0,
+  },
   view: state => (
     <main>
-      <h1>{state}</h1>
-      <button onclick={state => state - 1}>-</button>
-      <button onclick={state => state + 1}>+</button>
+      <h1>{state.title}</h1>
+      <h2>{state.num}</h2>
+      <button onclick={decrement}>-</button>
+      <button onclick={increment}>+</button>
     </main>
   ),
   node: document.getElementById("app")
